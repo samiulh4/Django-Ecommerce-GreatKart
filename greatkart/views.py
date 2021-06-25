6f816d4a4e260c2a931215097619962759d0b8e2
Md.Samiul  Haque <samiulh4@gmail.com>
2021-06-26 01:22:52 +0600

    completed 2-6

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Homepage')
+    return render(request, 'home.html')
