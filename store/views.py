ce4c47ef45b809d5e8f45b47c2578a2cab7d228b
Md.Samiul  Haque <samiulh4@gmail.com>
2021-07-04 01:35:33 +0600

    Starting 5

--- store/views.py
+++ store/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from store.models import Product
+from category.models import Category
 
-def store(request):
-    return render(request, 'store/store.html')
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True).order_by('created_date')
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('created_date')
+        product_count = products.count()
+
+    context ={
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
