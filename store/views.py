f7ff6bc02a18e6520ff2555120fdb73f16899556
Md.Samiul  Haque <samiulh4@gmail.com>
2021-07-11 23:40:47 +0600

    5-6 completed

--- store/views.py
+++ store/views.py
@@ -13,8 +13,21 @@ def store(request, category_slug=None):
         products = Product.objects.all().filter(is_available=True).order_by('created_date')
         product_count = products.count()
 
-    context ={
+    context = {
         'products': products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+
+    return render(request, 'store/product_detail.html', context)
